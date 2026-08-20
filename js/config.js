b739e049905ad0d25465319d98912c77fd517733
alonsoaz-sre <alonso.azaldegui@gmail.com>
2026-08-20 04:07:32 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,40 @@
 const CONFIG = {
-  whatsapp: "919293148"
+
+  whatsapp: "51919293148",
+
+  googleForm: {
+
+    url:
+      "https://docs.google.com/forms/d/e/1FAIpQLSd5cs84Qy2xb8PMbm9N6Jm0dPK2KSsQthjkN3tt0f0q7FK1sg/formResponse",
+
+    fields: {
+
+      fecha:
+        "entry.2083587791",
+
+      hora:
+        "entry.752415440",
+
+      nino:
+        "entry.1619075439",
+
+      adulto1:
+        "entry.1716065954",
+
+      adultos:
+        "entry.1198118450",
+
+      adulto2:
+        "entry.1961565983",
+
+      comentario:
+        "entry.2064474934",
+
+      origen:
+        "entry.1264424218"
+
+    }
+
+  }
+
 };
